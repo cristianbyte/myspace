a4ce0f111238138a191c5c160c546a900da8cde2
cristianbyte <charlsmaritz@gmail.com>
2024-08-06 12:23:10 -0500

    starting

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Banner from "./components/Banner.vue";
+</script>
+
+<template>
+    <Banner/>
+
+</template>
+
+<style>
+  body{
+    display: flex;
+    padding: 2% 0;
+    align-items: center;
+    justify-content: start;
+    flex-direction: column;
+    width: 100vw;
+    background-color: var(--background);
+    height: 100vh;
+  }
+</style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+    import CalendarIcon from './icons/Calendar.vue'
+    import LocationIcon from './icons/Location.vue'
+    import PaletteIcon from './icons/Palette.vue'
+    import imageProfile from '/profile.jpeg'
+
+</script>
+
+<template>
+    <div class="banner">
+        <div class="profile">
+            <div class="profile__image">
+                <img :src="imageProfile" alt="Profile Image">
+            </div>
+            <div class="profile__data">
+                <h3> Jhonson Dae</h3>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+   .banner {
+        position: relative;
+        display: flex;
+        align-items: end;
+        width: 80vw;
+        max-width: 1000px;
+        background-image: linear-gradient(50deg, var(--main) 30%, var(--accent)70%);
+        height: 30vh;
+        border-radius: 1vw;
+        overflow: hidden;
+        box-shadow: 0 0 .5rem var(--gray);
+   }
+
+   .profile{
+        height: 40%;
+        width: 100%;
+        padding: 0 2%;
+        background-color: var(--white);
+   }
+
+   .profile__image{
+        height: calc(100% + 2rem);
+        aspect-ratio: 1/1;
+        object-fit: fill;
+        transform: translateY(-40%);
+        z-index: 100;
+    }
+    .profile__image img{
+        width: 100%;
+        height: 100%;
+        border: .4rem solid var(--white);
+        border-radius: 50%;
+    }
+
+</style>
