cc6f94b55b6fbf6a848dd03caa88df225ae5171e
cristianbyte <charlsmaritz@gmail.com>
2024-08-08 10:32:10 -0500

    update: about completed

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -7,6 +7,8 @@
     import Phone from '../components/icons/Phone.vue';
     import Message from '../components/icons/Message.vue';
     import Mail from '../components/icons/Mail.vue';
+    import Network from '../components/icons/Network.vue';
+    import Projects from '../components/icons/Projects.vue';
 
     const name = 'John Doe';
     const status = 'Active';
@@ -20,48 +22,68 @@
 <template>
     <div class="profile">
         <div class="about">
-            <div class="about__field">
-                <h4>ABOUT</h4>
-                <IconText>
-                    <Account class="icon"/>
-                    Full Name: {{ name }}
-                </IconText>
-                <IconText>
-                    <Check class="icon"/>
-                    Status: {{ status }}
-                </IconText>
-                <IconText>
-                    <Country class="icon"/>
-                    Country: {{ country }}
-                </IconText>
-                <IconText>
-                    <Language class="icon"/>
-                    Language: {{ language }}
-                </IconText>
+            <div class="about__about">
+                <div class="about__field">
+                    <h4>ABOUT</h4>
+                    <IconText>
+                        <Account class="icon"/>
+                        Full Name: {{ name }}
+                    </IconText>
+                    <IconText>
+                        <Check class="icon"/>
+                        Status: {{ status }}
+                    </IconText>
+                    <IconText>
+                        <Country class="icon"/>
+                        Country: {{ country }}
+                    </IconText>
+                    <IconText>
+                        <Language class="icon"/>
+                        Language: {{ language }}
+                    </IconText>
+                </div>
+                <div class="about__field">
+                    <h4>Contacts</h4>
+                    <IconText>
+                        <Phone class="icon"/>
+                        Contact: {{ contact }}
+                    </IconText>
+                    <IconText>
+                        <Message class="icon"/>
+                        Skype: {{ nick }}
+                    </IconText>
+                    <IconText>
+                        <Mail class="icon"/>
+                        Email: {{ email }}
+                    </IconText>
+                </div>
+                <div class="about__field">
+                    <h4>teams</h4>
+                    <p>Backend Developer (126 Members)</p>
+                    <p>VueJS Developer (86 Members)</p>
+                </div>
             </div>
-            <div class="about__field">
-                <h4>Contacts</h4>
-                <IconText>
-                    <Phone class="icon"/>
-                    Contact: {{ contact }}
-                </IconText>
-                <IconText>
-                    <Message class="icon"/>
-                    Skype: {{ nick }}
-                </IconText>
-                <IconText>
-                    <Mail class="icon"/>
-                    Email: {{ email }}
-                </IconText>
-            </div>
-            <div class="about__field">
-                <h4>teams</h4>
-                <p>Backend Developer (126 Members)</p>
-                <p>VueJS Developer (86 Members)</p>
+            <div class="about__about">
+                <div class="about__field">
+                    <h4>overview</h4>
+                    <IconText>
+                        <Check class="icon"/>
+                        Task Complied: 13.5k
+                    </IconText>
+                    <IconText>
+                        <Network class="icon"/>
+                        Connections: 1024
+                    </IconText>
+                    <IconText>
+                        <Projects class="icon"/>
+                        Projects Complied: 13
+                    </IconText>
+
+                </div>
+
             </div>
         </div>
         <div class="activity">
-
         </div>
     </div>
 </template>
@@ -81,17 +103,33 @@
         justify-content: start;
         align-items: start;
         gap: 10px;
+        width: 30%;
         flex-direction: column;
         height: min-content;
-        width: min-content;
-        padding: 2%;
         color: var(--shadow);
+    }
+    
+    .about__about{
+        position: relative;
+        padding: 5%;
+        overflow: hidden;
+        width: 100%;
         background-color: var(--white);
-        border-radius: 5px;
         box-shadow: 0 0 .5rem var(--shadow);
+        border-radius: 5px;
     }
 
-    .about__field{
+    .about__field::before{
+        top: 0;
+        right: 0;
+        position: absolute;
+        content: '';
+        width: 10%;
+        height: 100%;
+        background-image: linear-gradient( 90deg, transparent 0%, var(--white) 100%);
+    }
+
+    .about__about, .about__field{
         display: flex;
         flex-direction: column;
         gap: 5px;
